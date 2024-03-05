9326039232c442709f8390e2c14579187702e93c
Brooklyn Sides <57104466+brooklynjsides@users.noreply.github.com>
2024-03-05 15:25:54 -0600

    testing desktop

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const dotenv = require('dotenv');
-
+const express = require("express");
+const dotenv = require("dotenv");
 
 const app = express();
 dotenv.config();
 
-
-app.get('/', (req, res) => {
-    res.send('Hello World');
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
 
 app.listen(PORT, () => {
-    console.log('Server is running on port 3000');
+  console.log("Server is running on port 3000");
 });
+
+//Desktop Test
